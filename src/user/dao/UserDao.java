efb7a5080b4aeee2666404bcac0fa1fff7083aa1
GoSungHwa <87704148+GoSungHwa@users.noreply.github.com>
2021-10-07 01:53:52 +0900

    회원가입 기능 & DB연동

--- src/user/dao/UserDao.java
+++ src/user/dao/UserDao.java
@@ -2,6 +2,7 @@ package user.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import user.vo.User;
 
@@ -12,7 +13,7 @@ public class UserDao {
 	}
 	public int insertUser(Connection conn,User vo) {
 		 int result= -1;
-		 String sql="insert into member(id,pw,uname,nickname,age,gender,email,phone,address,join_data,point,kind) values(?,?,?,?,?,?,?,?,?,?,0,'U')";
+		 String sql="insert into member(id,pw,uname,nickname,age,gender,email,phone,address,join_date,point,kind) values(?,?,?,?,?,?,?,?,?,sysdate,0,'U')";
 		 try {
 			 PreparedStatement pstmt=conn.prepareStatement(sql);
 			 pstmt.setString(1,vo.getUid());
@@ -20,16 +21,39 @@ public class UserDao {
 			 pstmt.setString(3,vo.getUname());
 			 pstmt.setString(4,vo.getNickname());
 			 pstmt.setInt(5,vo.getAge());
-			 pstmt.setLong(6,vo.getGender());
+			 pstmt.setString(6, String.valueOf(vo.getGender()));
 			 pstmt.setString(7,vo.getEmail());
 			 pstmt.setString(8,vo.getPhone());
 			 pstmt.setString(9,vo.getAddress());
-			 pstmt.setDate(10,vo.getJoin_date());
 			 result=pstmt.executeUpdate();
 		 }catch(Exception e){
 			 e.printStackTrace();	
 		 }
 		 return result;
 	}
+	
+	public User login(Connection conn,String id,String pw) {
+		User u=null;
+		
+		String sql="select pw from member where id=? and pw=?";
+		try {
+			PreparedStatement pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, id); 
+			 pstmt.setString(2, pw); 
+			ResultSet rs=pstmt.executeQuery();
+			
+			if(rs.next()) {
+				u=new User();
+				u.setUid(rs.getString("id"));
+				u.setPw(rs.getString("pw"));
+				}
+						
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return u;
+		
+	}
+	
 	
 }

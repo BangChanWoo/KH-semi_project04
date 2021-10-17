6193bf4f1f10d58a150c8c0a35a85187ff29666f
GoSungHwa <87704148+GoSungHwa@users.noreply.github.com>
2021-10-17 14:43:20 +0900

    로그아웃 기능 수정

--- src/user/controller/LogoutServlet.java
+++ src/user/controller/LogoutServlet.java
@@ -40,11 +40,14 @@ public class LogoutServlet extends HttpServlet {
 		process(request,response);
 	}
 	private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
+		HttpSession session = request.getSession(false);//false를 주면 현재 세션을 가지고 오는데 없으면 안가져옴
+		if(session!=null) {
 		session.invalidate();
+		}
 		
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/view/main.jsp");
-		rd.forward(request, response);		
+//		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/view/main.jsp");
+//		rd.forward(request, response);		
+		 response.sendRedirect("main"); 
 	}
 	
 
